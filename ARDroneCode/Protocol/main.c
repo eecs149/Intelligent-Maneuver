13de14bc047a97e458c38ff1c8d05097210f2eb6
Yoriyasu Yano <yorinasub17@gmail.com>
2012-04-06 09:34:30 -0700

    Added Protocol

---
+++ ARDroneCode/Protocol/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <pthread.h>
+
+#include <mrpt/utils.h>
+
+#include "app.h" 
+
+extern float32_t angles[3];
+extern float32_t velocities[3];
+
+extern pthread_mutex_t mutex;
+
+int main( int argc, char* argv[] )
+{
+   appInit();
+
+   while (1) 
+       ;
+
+   return 0;
+}
+
